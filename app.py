ad129fdb6a3a4d697aa342fd6a7f10bef2f13dbf
sachinpr0001 <sachinpr0001@gmail.com>
2022-10-09 00:20:58 +0530

    added venv

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
+"""Flask app for review sentiment analyser
+
+Returns:
+    html: index.html
+"""
 import csv
 from flask import Flask, render_template, request
 import pandas as pd
@@ -5,25 +10,34 @@ from model import get_review
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET','POST'])
-def index():
-    return render_template('index.html')
 
-@app.route('/data', methods=['GET','POST'])
+@app.route("/", methods=["GET", "POST"])
+def index():
+    """routing method
+
+    Returns:
+        html
+    """
+    return render_template("index.html")
+
+
+@app.route("/data", methods=["GET", "POST"])
 def csv_to_df():
-    if request.method == 'POST':
-        inputcsv = request.form['csvfile']
-        data=[]
-        with open(inputcsv, encoding = 'utf8') as file:
+    """converts csv to df
+    """
+    if request.method == "POST":
+        inputcsv = request.form["csvfile"]
+        data = []
+        with open(inputcsv, encoding="utf8") as file:
             csvfile = csv.reader(file)
             for row in csvfile:
                 data.append(row)
         print(type(data))
         dataframe = pd.DataFrame(data)
-        dataframe.to_csv('sample.csv',header=False, index = False)
-        dataframe = pd.read_csv('sample.csv')
+        dataframe.to_csv("sample.csv", header=False, index=False)
+        dataframe = pd.read_csv("sample.csv")
         review = get_review(dataframe)
-        return render_template('data.html', data=review.to_html(header=True,index=False))
+    return render_template("data.html", data=review.to_html(header=True, index=False))
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
--- model.py
+++ model.py
@@ -1,21 +1,35 @@
+"""module for flask app
+
+Returns:
+    DataFrame
+"""
 import re
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
 
+
 def get_review(dataframe):
+    """returns dataframe to flask app
+
+    Args:
+        dataframe (pd.DataFrame)
+
+    Returns:
+        dataframe: pd.DataFrame
+    """
     lemmatizer = WordNetLemmatizer()
     all_reviews = dataframe
     print(all_reviews.head())
 
-    sw_clean = set(stopwords.words('english'))
-    sw_clean.remove('not')
-    sw_clean.remove('no')
+    sw_clean = set(stopwords.words("english"))
+    sw_clean.remove("not")
+    sw_clean.remove("no")
     new_review = []
-    for review in all_reviews['Text']:
-        review = re.sub(r'[^\w\s]', '', str(review))
-        review = re.sub(r'\d','',review)
+    for review in all_reviews["Text"]:
+        review = re.sub(r"[^\w\s]", "", str(review))
+        review = re.sub(r"\d", "", review)
         review_token = word_tokenize(review.lower().strip())
         r_without_stop = []
         for word in review_token:
@@ -24,17 +38,17 @@ def get_review(dataframe):
                 r_without_stop.append(word)
         clean_review = " ".join(r_without_stop)
         new_review.append(clean_review)
-    all_reviews['New_Text'] = new_review
+    all_reviews["New_Text"] = new_review
     req_review = all_reviews[all_reviews.Star <= 3]
     sentiment_model = SentimentIntensityAnalyzer()
     senti_list = []
-    for review in req_review['New_Text']:
+    for review in req_review["New_Text"]:
         score = sentiment_model.polarity_scores(review)
-        if score['pos'] >= 0.7:
-            senti_list.append('+')
+        if score["pos"] >= 0.7:
+            senti_list.append("+")
         else:
-            senti_list.append('-')
+            senti_list.append("-")
     req_review["sentiment"] = senti_list
-    final_review = req_review[req_review.sentiment == '+']
-    final_review.drop('New_Text', axis = 1,inplace=True)
+    final_review = req_review[req_review.sentiment == "+"]
+    final_review.drop("New_Text", axis=1, inplace=True)
     return final_review
